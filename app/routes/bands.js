88a18e0b85806c97ba431a6755ac5f546625f7d0
ilishchinovskiy <i.lishchinovskiy@svitla.com>
2015-09-19 21:37:11 +0300

    Chapters 3-5 RaRwE

---
+++ app/routes/bands.js
@@ -0,0 +1,17 @@
+var Band = Ember.Object.extend({
+  name: '',
+});
+
+var ledZeppelin = Band.create({ name: 'Led Zeppelin' });
+var pearlJam = Band.create({ name: 'Pearl Jam' });
+var fooFighters = Band.create({ name: 'Foo Fighters' });
+
+var bands = [ledZeppelin, pearlJam, fooFighters];
+
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function() {
+    return bands;
+  }
+});
